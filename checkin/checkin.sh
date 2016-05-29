2bee18ce29f25be31c16a2f5e391c529d983f672
hello <hello@hub-16.04-base>
2016-05-29 16:15:40 +0300

    updated checkin script to work with cron

--- checkin/checkin.sh
+++ checkin/checkin.sh
@@ -3,5 +3,5 @@ HOST=$(hostname --long)
 
 touch /tmp/$HOST
 uptime -p > /tmp/$HOST
-scp -i /home/hello/.ssh/id_rsa /tmp/$HOST uganda@hellohub.org:/home/kiosk/public_html/status/updates/$HOST
+sudo -u hello scp -i /home/hello/.ssh/id_rsa /tmp/$HOST uganda@hellohub.org:/home/kiosk/public_html/status/updates/$HOST
 rm /tmp/$HOST
